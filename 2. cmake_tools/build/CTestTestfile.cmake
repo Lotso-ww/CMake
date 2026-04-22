2f815ddfe8f22555c8ceb39f60697f52d30cb0ef
Lotso <3208137151@qq.com>
2026-04-22 17:37:54 +0800

    cmake的使用

---
+++ 2. cmake_tools/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/lotso/118/cmake/2. cmake_tools
+# Build directory: /home/lotso/118/cmake/2. cmake_tools/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Name "Case_Add" "COMMAND" "testAdd")
+set_tests_properties(Name PROPERTIES  _BACKTRACE_TRIPLES "/home/lotso/118/cmake/2. cmake_tools/CMakeLists.txt;14;add_test;/home/lotso/118/cmake/2. cmake_tools/CMakeLists.txt;0;")
---
+++ 2. cmake_tools/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <assert.h>
+
+int main()
+{
+    assert(1 + 2 == 3);
+    std::cout << "test passed!" << std::endl;
+    return 0;
+}
