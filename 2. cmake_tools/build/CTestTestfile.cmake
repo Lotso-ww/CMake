a360f36f7e7a5d4b1fbe2748ee5f0e59f6e9a014
Lotso <3208137151@qq.com>
2026-04-23 10:03:18 +0800

    cmake_tools

--- 2. cmake_tools/build/CTestTestfile.cmake
+++ 2. cmake_tools/build/CTestTestfile.cmake
@@ -4,5 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(Name "Case_Add" "COMMAND" "testAdd")
-set_tests_properties(Name PROPERTIES  _BACKTRACE_TRIPLES "/home/lotso/118/cmake/2. cmake_tools/CMakeLists.txt;14;add_test;/home/lotso/118/cmake/2. cmake_tools/CMakeLists.txt;0;")
+add_test(Case_Add "/home/lotso/118/cmake/2. cmake_tools/build/testAdd")
+set_tests_properties(Case_Add PROPERTIES  _BACKTRACE_TRIPLES "/home/lotso/118/cmake/2. cmake_tools/CMakeLists.txt;14;add_test;/home/lotso/118/cmake/2. cmake_tools/CMakeLists.txt;0;")
--- 2. cmake_tools/test.cpp
+++ 2. cmake_tools/test.cpp
@@ -4,6 +4,7 @@
 int main()
 {
     assert(1 + 2 == 3);
+    // assert(1 + 2 == 4);
     std::cout << "test passed!" << std::endl;
     return 0;
 }
