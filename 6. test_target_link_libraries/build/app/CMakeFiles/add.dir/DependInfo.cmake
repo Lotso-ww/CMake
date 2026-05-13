23f8b5ee48627d23f8a5a5baf5bfce3820ab6d8c
Lotso <3208137151@qq.com>
2026-05-13 21:33:32 +0800

    6. test_target_link_libraries

---
+++ 6. test_target_link_libraries/build/app/CMakeFiles/add.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lotso/118/cmake/6. test_target_link_libraries/app/add.cpp" "app/CMakeFiles/add.dir/add.cpp.o" "gcc" "app/CMakeFiles/add.dir/add.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
