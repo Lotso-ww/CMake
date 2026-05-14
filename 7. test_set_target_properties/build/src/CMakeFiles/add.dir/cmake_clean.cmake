3669e0ff7be187e8c84a551d03a20e7b7bcecaa1
Lotso <3208137151@qq.com>
2026-05-14 15:37:05 +0800

    7. test_set_target_properties

---
+++ 7. test_set_target_properties/build/src/CMakeFiles/add.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "../lib/.20"
+  "../lib/libadd.pdb"
+  "../lib/libadd.so"
+  "../lib/libadd.so.1.2.3"
+  "../lib/libadd.so.20"
+  "CMakeFiles/add.dir/add.cpp.o"
+  "CMakeFiles/add.dir/add.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/add.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
